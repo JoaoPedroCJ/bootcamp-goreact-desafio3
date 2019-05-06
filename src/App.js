2d881f20bb9501dc8ac639d7a7b80dfb4635751a
João Pedro <joaopedrocj@gmail.com>
2019-05-06 15:26:57 -0300

    Rocketseat GoReact - Desafio 3

--- src/App.js
+++ src/App.js
@@ -1,11 +1,27 @@
-import React from 'react';
+import dotenv from 'dotenv';
+import React, { Fragment } from 'react';
+import './config/ReactotronConfig';
 
-function App() {
-  return (
-    <div className="App">
-      <h1>Hello World</h1>
-    </div>
-  );
-}
+import { ToastContainer } from 'react-toastify';
+import 'react-toastify/dist/ReactToastify.css';
+
+import { Provider } from 'react-redux';
+import store from './store';
+
+import Routes from './routes';
+
+import 'font-awesome/css/font-awesome.css';
+import './styles.scss';
+
+dotenv.config();
+
+const App = () => (
+  <Provider store={store}>
+    <Fragment>
+      <Routes />
+      <ToastContainer autoClose={5000} />
+    </Fragment>
+  </Provider>
+);
 
 export default App;
